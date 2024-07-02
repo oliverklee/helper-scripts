d78965fe13b06cc9728c32d8bda81fd5b297e843
Oliver Klee <github@oliverklee.de>
2024-07-02 17:38:27 +0200

    [BUGFIX] Avoid turning ANSI colors off
    The environment variable `NO_COLOR` has a predefined meaning.
    
    Also, the official terms for going back to the default color
    is "color off", not "no color".

--- output.sh
+++ output.sh
@@ -13,15 +13,15 @@ function linefeed {
 
 # Outputs the provided string as a success status message.
 function success {
-  echo -e "${GREEN}$1${NO_COLOR}"
+  echo -e "${GREEN}$1${COLOR_OFF}"
 }
 
 # Outputs the provided string as an error status message.
 function error {
-  echo -e "${RED}$1${NO_COLOR}"
+  echo -e "${RED}$1${COLOR_OFF}"
 }
 
 # Outputs the provided string as a notice status message.
 function notice {
-  echo -e "${YELLOW}$1${NO_COLOR}"
+  echo -e "${YELLOW}$1${COLOR_OFF}"
 }
